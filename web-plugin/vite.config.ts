ff26363164ef49300aaf38a4142f3113972e411b
anteqkois <anteqkois.dev@gmail.com>
2024-07-05 11:49:24 +0200

    (build) not working dou too nodejs process

--- web-plugin/vite.config.ts
+++ web-plugin/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
-import dts from "vite-plugin-dts";
+// import dts from "vite-plugin-dts";
 import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
@@ -12,28 +12,29 @@ export default defineConfig({
       name: "webplugin",
       // fileName: (format) => `index.${format}.js`,
     },
-    rollupOptions: {
-      external: [
-        "react",
-        // "react-dom",
-        // "tailwindcss"
-      ],
-      output: {
-        paths: {
-          react: "https://unpkg.com/react@16/umd/react.production.min.js",
-          "react-dom": "https://unpkg.com/react-dom@16/umd/react-dom.production.min.js",
-        },
-        // globals: {
-        //   react: "React",
-        //   "react-dom": "ReactDOM",
-        //   tailwindcss: "tailwindcss",
-        // },
-      },
-    },
-    sourcemap: true,
+    // rollupOptions: {
+    //   external: [
+    //     "react",
+    //     "react-dom",
+    //     // "tailwindcss"
+    //   ],
+    //   output: {
+    //     paths: {
+    //       react: "https://unpkg.com/react@16/umd/react.production.min.js",
+    //       "react-dom": "https://unpkg.com/react-dom@16/umd/react-dom.production.min.js",
+    //     },
+    //     // globals: {
+    //     //   react: "React",
+    //     //   "react-dom": "ReactDOM",
+    //     //   tailwindcss: "tailwindcss",
+    //     // },
+    //   },
+    // },
+    // sourcemap: true,
     emptyOutDir: true,
   },
-  plugins: [react(), dts({ rollupTypes: true })],
+  // plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss],
