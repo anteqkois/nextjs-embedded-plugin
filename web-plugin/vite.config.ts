4d83c9ece28f3fdea0ec46c6eb49cfbba8ccb62d
anteqkois <anteqkois.dev@gmail.com>
2024-07-05 11:27:47 +0200

    (build) bundle with external react

--- web-plugin/vite.config.ts
+++ web-plugin/vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./lib/main.ts"),
+      name: "webplugin",
+      // fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: [
+        "react",
+        // "react-dom",
+        // "tailwindcss"
+      ],
+      output: {
+        paths: {
+          react: "https://unpkg.com/react@16/umd/react.production.min.js",
+          "react-dom": "https://unpkg.com/react-dom@16/umd/react-dom.production.min.js",
+        },
+        // globals: {
+        //   react: "React",
+        //   "react-dom": "ReactDOM",
+        //   tailwindcss: "tailwindcss",
+        // },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+});
