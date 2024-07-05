6e5c4431a7fca375e8b19a83016442483c567614
anteqkois <anteqkois.dev@gmail.com>
2024-07-05 09:40:24 +0200

    (lib) bundle only react lib

--- react-plugin/vite.config.ts
+++ react-plugin/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), libInjectCss()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+    copyPublicDir: false,
+    // minify: true,
+    // rollupOptions: {
+    //   external: ["react", "react/jsx-runtime"],
+    // },
+  },
+});
